59f1c57a6ca698625cb2614914ca90416c1610df
PranavSaiyan <pranavsupersaiyan10@gmail.com>
2019-04-11 19:01:12 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  user1Activated = false;
+  user2Activated = false;
+  constructor(private usrserv: UsersService) { }
+  ngOnInit() {
+    this.usrserv.userActivated.subscribe((id: number) => {
+      if (id === 1) {
+        this.user1Activated = true;
+      } else if (id === 2) {
+        this.user2Activated = true;
+      }
+    });
+  }
 }
