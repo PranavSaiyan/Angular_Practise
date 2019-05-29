c58308258bb79ddbe4f11f53ea8cf1feca3f466f
PranavSaiyan <pranavsupersaiyan10@gmail.com>
2019-05-29 11:46:55 +0530

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ countSub: Subscription;
     this.countSub.unsubscribe();
   }
   ngOnInit() {
-    const myNumbers = Observable.interval(1000).map((data:number)=>{ return 10*data});
+    const myNumbers = Observable.interval(1000).map((data:number)=>{ return 1*data});
     this.countSub = myNumbers.subscribe((n:number)=>{
       this.no=n;
     })
